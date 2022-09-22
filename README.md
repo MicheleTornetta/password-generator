50bbc41034c99eb3fe347dafcbb8b2aacd056789
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2022-09-21 22:14:17 -0400

    vanishing button

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
-# password-generator
+# Password-Generator
 
+## Description
 
+The Password-Generator was designed to generate random passwords using criteria the user has chosen.
 
+## User Story
 
+The user works for a company that requires passwords be changed frequently, and they want a password that is very secure by generating random letters, numbers and/or symbols. The Password-Generator will do just that.
 
+## Acceptance Criteria
 
+WHEN User clicks the "Click Here" button
+THEN a form will open up allowing the user choose the length of the password and if they wish to include lowercase, uppercase, numeric, and/or special characters
+WHEN User Clics on the "Generate Password" button
+THEN the user's input is validated and at least one character type must be selected
+WHEN all prompts are answered correctly
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
 
+## Assets
 
+The following images demonstrates the web application's appearance and functionality:
 
-
-
-
-
-
-
-
+![The portfolio page features headings in blue text, rounded corners on the image placeholder, and various background colors.](./assets/image-1.png)
 
 ### Background Photo
-[!Photo by T.H. Chia on Unsplash] <a href="https://unsplash.com/backgrounds/cool?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"></a>
+
+[Picture of the Password-Generator] <a href="https://unsplash.com/backgrounds/cool?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"></a>
--- script.js
+++ script.js
@@ -1,83 +1,83 @@
-//tips:  Look at random numbers, alerts, bootstrap model, can use forms, have to get user input (4 ways - form or alerts and confirms)
-//will need 2 for loops
-//get booliean
+//Code to show html form and validate u
 
-let likeSymbols = confirm("Do you want symbols?"); //will get you a true/false
-let likeLetters = confirm("Do you want letters?"); //will get a true/false
-let likeNumbers = confirm("Do you want numbers?"); //will get a true/false
-let length = Number(
-  prompt(
-    "How long would you like your password to be?  Pick a number between 8 and 128."
-  )
-);
-while (isNaN(length) || length < 8 || length > 128) {
-  //3 conditions to ensure number entered, and is between 8 and 128
-  length = Number(
-    prompt("Try again, you need to pick a number between 8 and 128.")
-  ); //keep running until they get it right
+function generatePassword() {
+  let generatelengthElement = document.getElementById("pwlength");
+  let generatelettersElement = document.getElementById("letters");
+  let generateupLettersElement = document.getElementById("upLetters");
+  let generatenumbersElement = document.getElementById("numbers");
+  let generatesymbolsElement = document.getElementById("symbols");
+
+  let length = Number(generatelengthElement.value);
+  let likeLowerLetters = generatelettersElement.checked;
+  let likeUpperLetters = generateupLettersElement.checked;
+  let likeNumbers = generatenumbersElement.checked;
+  let likeSymbols = generatesymbolsElement.checked;
+
+  if (!(likeSymbols || likeLowerLetters || likeUpperLetters || likeNumbers)) {
+    alert(
+      "Please choose at least one type of character to generate a password."
+    );
+    return;
+  }
+
+  //console.log (length);
+
+  //items that can be used for passwords
+  const symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  const numbers = "0123456789";
+  const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+  const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  //confirm what items the client wanted to use = true
+  let pool = "";
+  if (likeSymbols) {
+    pool += symbols;
+  }
+  if (likeLowerLetters) {
+    pool += lowerLetters;
+  }
+  if (likeUpperLetters) {
+    pool += upperLetters;
+  }
+  if (likeNumbers) {
+    pool += numbers;}
+   {
+      display: none;
+    }
+
+  console.log(pool);
+
+  let passwordGeneration = "";
+
+  for (let counter = 0; counter < length; counter += 1) {
+    passwordGeneration += pool[Math.floor(Math.random() * pool.length)];
+  }
+  // math.floor means to round down the random number that will be between 0 and .999999 and multiply that by the number of items in the variable
+
+  return passwordGeneration;
 }
 
-//console.log (length);
+// Start of form for password questions
 
-//items that can be used for passwords
-const symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-const numbers = "0123456789";
-const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-//confirm what items the client wanted to use = true
-let pool = "";
-if (likeSymbols) {
-  pool += symbols;
-}
-if (likeLetters) {
-  pool += letters;
-}
-if (likeNumbers) {
-  pool += numbers;
+function showform() {
+  let formElement = document.getElementById("form");
+  formElement.style.display = "block";
+  let formButton = document.getElementById("formcall");
+  formButton.classList.add("hidden");
 }
 
-console.log (pool)
+let formcall = document.getElementById("formcall");
+formcall.addEventListener("click", showform);
 
-for (let counter = 0; counter < length; counter += 1) {
-  
+function writePassword(event) {
+  event.preventDefault();
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  if (password !== undefined) {
+    passwordText.value = password;
+  }
 }
-
-pool[Math.floor(Math.random() * pool.length)]; 
-
-
-// math.floor means to round down the random number that will be between 0 and .999999 and multiply that by the number of items in the variable
-
-/*
-    //or use a form (above Generate Password)
-
-//need a random number generator
- let myName
-
-//Object characters that can be used
-
-function random
-
-
-
-// Assignment Code
-
 
 let generateBtn = document.getElementById("generate");
-// generateBtn.innerText = 'Helloooo!';
-
-console.log(generateBtn.innerText);
-
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword(Length);
-  
-  let passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-*/
